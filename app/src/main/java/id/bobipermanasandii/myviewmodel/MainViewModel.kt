3d949165b760cd2f83e24787aec401eb52fabde6
Bobi Permana Sandi <bobipermana17@gmail.com>
2024-03-19 20:48:20 +0700

    first commit

---
+++ app/src/main/java/id/bobipermanasandii/myviewmodel/MainViewModel.kt
@@ -0,0 +1,12 @@
+package id.bobipermanasandii.myviewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    var result = 0
+
+    fun calculate(width: String, height: String, length: String) {
+        result = width.toInt() * height.toInt() * length.toInt()
+    }
+}
